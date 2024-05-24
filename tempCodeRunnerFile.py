108df6da26d584504abf37c087011c1523e28734
HanishDhanwalkar <hanishpd1729@gmail.com>
2024-05-24 17:41:22 +0530

    removed redendened

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-mlm = MLM(data= "data/short/100sentences_eng.txt")
-mlm.start()
-
-pred, string = mlm.predict("Diagnosis [MASK] may not know you have atrial fibrillation AFib")
-
-print(pred)
-print(string)
